03c227cf61bdd3c0333dd994cfd80cad5ba78c38
Quan Nguyen <nguyen.hong.quan282@gmail.com>
2020-10-30 16:50:05 +0700

    basic setup

---
+++ src/app/app-routing.module.ts
@@ -0,0 +1,19 @@
+import { NgModule } from '@angular/core';
+import { RouterModule } from '@angular/router';
+
+import { Routes } from '@angular/router';
+
+const RoutesConfig : Routes = [
+    {path : 'home' , loadChildren: () => import('./module/home/home.module').then(m => m.HomeModule) }, 
+    //{path : '**' , redirectTo : '/home' , pathMatch: 'full'}
+]
+
+@NgModule({
+  imports: [
+    RouterModule.forRoot(RoutesConfig)
+  ],
+
+  exports: [RouterModule]
+
+})
+export class AppRoutingModule { }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,13 +2,17 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { AppRoutingModule } from './app-routing.module';
+import { CommonModule } from '@angular/common';
 
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    CommonModule,
+    AppRoutingModule
   ],
   providers: [],
   bootstrap: [AppComponent]
